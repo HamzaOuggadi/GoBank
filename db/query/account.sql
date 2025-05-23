fef5f1f5d42e3139c1c29e2d6033b24054a7a777
hamza <hamza.ouggadi@gmail.com>
2025-05-23 22:47:46 +0100

    //

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,4 +1,4 @@
--- name CreateAccount :one
+-- name: CreateAccount :one
 INSERT INTO accounts (
     owner,
     balance,
@@ -7,6 +7,20 @@ INSERT INTO accounts (
     $1, $2, $3
 ) RETURNING *;
 
--- name GetAccounts :one
+-- name: ListAccounts :many
+SELECT * from accounts
+ORDER BY id
+LIMIT $1;
+
+-- name: GetAccountById :one
 SELECT * from accounts
 WHERE id=$1 LIMIT 1;
+
+-- name: UpdateAccountBalance :exec
+UPDATE accounts
+SET balance = $2
+WHERE id = $1;
+
+-- name: DeleteAccountById :exec
+DELETE FROM accounts
+WHERE id = $1;
